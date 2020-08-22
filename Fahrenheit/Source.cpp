fc5f4d83b400d4ddb734cd50cd810c48148c067f
YANISA PONGMETHA <63010249@KMITL.AC.TH>
2020-08-22 08:52:28 +0700

    ...

---
+++ Fahrenheit/Source.cpp
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+
+int main() {
+	float F,C ;
+	printf("Input Fahrenheit : ");
+	scanf_s("%f", &F);
+	C = (5 * (F - 32))/9 ;
+	if (F >= 32) printf("Celsius = %.2f", C);
+	else printf("Too cold to live");
+
+}
